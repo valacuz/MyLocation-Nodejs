855b71c6da938c682a1ca27021bdf9845c0975ea
Kritpapon Thitichaimongkhol <valacuz@gmail.com>
2018-07-15 23:06:45 +0700

    Fix process environment port variable name

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
 
-const port = process.env.port || 8124
+const port = process.env.PORT || 8124
 const app = express()
 
 // Wear helmet saves lives :)
