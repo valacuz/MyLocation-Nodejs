2cd340ba668958eca02216c4fcc9da8c05b2b305
Kritpapon Thitichaimongkhol <valacuz@gmail.com>
2018-08-21 22:39:39 +0700

    Change the way to load local configuration

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
-const local = require('./local-config')
+const env = require('env2')('./config/local-config.env')
 
 module.exports = {
     // Server configuration here
-    listenPort: process.env.PORT || local.listenPort,
-    mongoDbUrl: process.env.MONGO_URL || local.mongoDbUrl,
-    mongoDbUser: process.env.MONGO_USER || local.mongoDbUser,
-    mongoDbPwd: process.env.MONGO_PWD || local.mongoDbPwd
+    listenPort: process.env.PORT,
+    mongoDbUrl: process.env.MONGO_URL,
+    mongoDbUser: process.env.MONGO_USER,
+    mongoDbPwd: process.env.MONGO_PWD
 }
