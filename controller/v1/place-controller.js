cfada6593095d65692ddf3f11d9df85e6e80875e
Kritpapon Thitichaimongkhol <valacuz@gmail.com>
2018-09-16 02:31:52 +0700

    refactor controller to conform with javascript standard style

--- controller/v1/place-controller.js
+++ controller/v1/place-controller.js
@@ -1,4 +1,4 @@
-"use strict"
+'use strict'
 
 const PlaceSource = require('./../../model/source/place')
 const UserSource = require('./../../model/source/user')
@@ -12,164 +12,164 @@ const placeSource = new PlaceSource()
 const PlaceController = function () { }
 
 PlaceController.prototype.getPlaces = async (request, response) => {
-    const payload = request.payload
-    var [err, user] = await to(userSource.checkUser(payload.user, payload.password))
-    if (err) {
-        // any error, response unavailable.
-        response.sendStatus(503)
-        return
-    } else if (!user) {
-        // user not found due to invalid information, response unauthorized.
-        response.sendStatus(401)
-        return
-    }
-    const offset = request.query.offset !== undefined ? Number(request.query.offset) : 0
-    const limit = request.query.limit !== undefined ? Number(request.query.limit) : 20
-    var [err, places] = await to(placeSource.getPlaces(offset, limit))
-    if (err) {
-        response.sendStatus(503)
-        return
-    }
-    // query successful, response ok with json array of places.
-    response.json(places)
+  const payload = request.payload
+  var [userErr, user] = await to(userSource.checkUser(payload.user, payload.password))
+  if (userErr) {
+    // any error, response unavailable.
+    response.sendStatus(503)
+    return
+  } else if (!user) {
+    // user not found due to invalid information, response unauthorized.
+    response.sendStatus(401)
+    return
+  }
+  const offset = request.query.offset !== undefined ? Number(request.query.offset) : 0
+  const limit = request.query.limit !== undefined ? Number(request.query.limit) : 20
+  var [queryErr, places] = await to(placeSource.getPlaces(offset, limit))
+  if (queryErr) {
+    response.sendStatus(503)
+    return
+  }
+  // query successful, response ok with json array of places.
+  response.json(places)
 }
 
 PlaceController.prototype.getPlaceById = async (request, response) => {
-    const payload = request.payload
-    var [err, user] = await to(userSource.checkUser(payload.user, payload.password))
-    if (err) {
-        // any error, response unavailable.
-        response.sendStatus(503)
-        return
-    } else if (!user) {
-        // user not found due to invalid information, response unauthorized.
-        response.sendStatus(401)
-        return
-    }
-    var [err, place] = await to(placeSource.getPlaceById(request.params.id))
-    if (err) {
-        response.sendStatus(503)
-        return
-    } else if (!place) {
-        // place not found, response not found.
-        response.sendStatus(404)
-        return
-    }
-    // query successful, response ok with json of place.
-    response.json(place)
+  const payload = request.payload
+  var [userErr, user] = await to(userSource.checkUser(payload.user, payload.password))
+  if (userErr) {
+    // any error, response unavailable.
+    response.sendStatus(503)
+    return
+  } else if (!user) {
+    // user not found due to invalid information, response unauthorized.
+    response.sendStatus(401)
+    return
+  }
+  var [queryErr, place] = await to(placeSource.getPlaceById(request.params.id))
+  if (queryErr) {
+    response.sendStatus(503)
+    return
+  } else if (!place) {
+    // place not found, response not found.
+    response.sendStatus(404)
+    return
+  }
+  // query successful, response ok with json of place.
+  response.json(place)
 }
 
 PlaceController.prototype.addPlace = async (request, response) => {
-    const payload = request.payload
-    var [err, user] = await to(userSource.checkUser(payload.user, payload.password))
-    if (err) {
-        // any error, response unavailable.
-        response.sendStatus(503)
-        return
-    } else if (!user) {
-        // user not found due to invalid information, response unauthorized.
-        response.sendStatus(401)
-        return
-    } else if (!user.can_insert) {
-        // user not allow to insert, response forbidden.
-        response.sendStatus(403)
-        return
-    }
-    const body = request.body
-    const contentType = request.headers['content-type']
-    const result = new Validator().validatePlace(body)
-    if (contentType != CONTENT_TYPE_JSON || result.error) {
-        // content not provided or invalid form of request, response bad request.
-        response.sendStatus(400)
-        return
-    }
-    var [err, newPlace] = await to(placeSource.addPlace(body))
-    if (err) {
-        response.sendStatus(503)
-        return
-    }
-    // creation successful, response created.
-    response.status(201)
-        .location(`/places/${newPlace.place_id}`)
-        .contentType(CONTENT_TYPE_JSON)
-        .send(newPlace)
+  const payload = request.payload
+  var [userErr, user] = await to(userSource.checkUser(payload.user, payload.password))
+  if (userErr) {
+    // any error, response unavailable.
+    response.sendStatus(503)
+    return
+  } else if (!user) {
+    // user not found due to invalid information, response unauthorized.
+    response.sendStatus(401)
+    return
+  } else if (!user.can_insert) {
+    // user not allow to insert, response forbidden.
+    response.sendStatus(403)
+    return
+  }
+  const body = request.body
+  const contentType = request.headers['content-type']
+  const result = new Validator().validatePlace(body)
+  if (contentType !== CONTENT_TYPE_JSON || result.error) {
+    // content not provided or invalid form of request, response bad request.
+    response.sendStatus(400)
+    return
+  }
+  var [addErr, newPlace] = await to(placeSource.addPlace(body))
+  if (addErr) {
+    response.sendStatus(503)
+    return
+  }
+  // creation successful, response created.
+  response.status(201)
+    .location(`/places/${newPlace.place_id}`)
+    .contentType(CONTENT_TYPE_JSON)
+    .send(newPlace)
 }
 
 PlaceController.prototype.updatePlace = async (request, response) => {
-    const payload = request.payload
-    var [err, user] = await to(userSource.checkUser(payload.user, payload.password))
-    if (err) {
-        // any error, response unavailable.
-        response.sendStatus(503)
-        return
-    } else if (!user) {
-        // user not found due to invalid information, response unauthorized.
-        response.sendStatus(401)
-        return
-    } else if (!user.can_update) {
-        // user not allow to update, response forbidden.
-        response.sendStatus(403)
-        return
-    }
-    const body = request.body
-    const contentType = request.headers['content-type']
-    const queryString = request.params
-    const result = new Validator().validatePlace(body)
-    if (queryString.id != body.place_id || contentType != CONTENT_TYPE_JSON || result.error) {
-        // content not provided or invalid form of request, response bad request.
-        response.sendStatus(400)
-        return
-    }
-    var [err, place] = await to(placeSource.getPlaceById(queryString.id))
-    if (err) {
-        response.sendStatus(503)
-        return
-    } else if (!place) {
-        // place to updated not found, response not found.
-        response.sendStatus(404)
-        return
-    }
-    var [err] = await to(placeSource.updatePlace(body))
-    if (err) {
-        response.sendStatus(503)
-        return
-    }
-    // updation successful, response ok.
-    response.sendStatus(200)
+  const payload = request.payload
+  var [userErr, user] = await to(userSource.checkUser(payload.user, payload.password))
+  if (userErr) {
+    // any error, response unavailable.
+    response.sendStatus(503)
+    return
+  } else if (!user) {
+    // user not found due to invalid information, response unauthorized.
+    response.sendStatus(401)
+    return
+  } else if (!user.can_update) {
+    // user not allow to update, response forbidden.
+    response.sendStatus(403)
+    return
+  }
+  const body = request.body
+  const contentType = request.headers['content-type']
+  const queryString = request.params
+  const result = new Validator().validatePlace(body)
+  if (queryString.id !== body.place_id || contentType !== CONTENT_TYPE_JSON || result.error) {
+    // content not provided or invalid form of request, response bad request.
+    response.sendStatus(400)
+    return
+  }
+  var [queryErr, place] = await to(placeSource.getPlaceById(queryString.id))
+  if (queryErr) {
+    response.sendStatus(503)
+    return
+  } else if (!place) {
+    // place to updated not found, response not found.
+    response.sendStatus(404)
+    return
+  }
+  var [updateErr] = await to(placeSource.updatePlace(body))
+  if (updateErr) {
+    response.sendStatus(503)
+    return
+  }
+  // updation successful, response ok.
+  response.sendStatus(200)
 }
 
 PlaceController.prototype.deletePlace = async (request, response) => {
-    const payload = request.payload
-    var [err, user] = await to(userSource.checkUser(payload.user, payload.password))
-    if (err) {
-        response.sendStatus(503)
-        return
-    } else if (!user) {
-        // user not found due to invalid information, response unauthorized.
-        response.sendStatus(401)
-        return
-    } else if (!user.can_delete) {
-        // user not allow to delete, response forbidden.
-        response.sendStatus(403)
-        return
-    }
-    const queryString = request.params
-    var [err, place] = await to(placeSource.getPlaceById(queryString.id))
-    if (err) {
-        response.sendStatus(503)
-        return
-    } else if (!place) {
-        // place not found, response not found.
-        response.sendStatus(404)
-        return
-    }
-    var [err] = await to(placeSource.deletePlace(queryString.id))
-    if (err) {
-        response.sendStatus(503)
-        return
-    }
-    // deletion successful, response no content.
-    response.sendStatus(204)
+  const payload = request.payload
+  var [userErr, user] = await to(userSource.checkUser(payload.user, payload.password))
+  if (userErr) {
+    response.sendStatus(503)
+    return
+  } else if (!user) {
+    // user not found due to invalid information, response unauthorized.
+    response.sendStatus(401)
+    return
+  } else if (!user.can_delete) {
+    // user not allow to delete, response forbidden.
+    response.sendStatus(403)
+    return
+  }
+  const queryString = request.params
+  var [queryErr, place] = await to(placeSource.getPlaceById(queryString.id))
+  if (queryErr) {
+    response.sendStatus(503)
+    return
+  } else if (!place) {
+    // place not found, response not found.
+    response.sendStatus(404)
+    return
+  }
+  var [deleteErr] = await to(placeSource.deletePlace(queryString.id))
+  if (deleteErr) {
+    response.sendStatus(503)
+    return
+  }
+  // deletion successful, response no content.
+  response.sendStatus(204)
 }
 
 module.exports = PlaceController
--- controller/v1/type-controller.js
+++ controller/v1/type-controller.js
@@ -1,4 +1,4 @@
-"use strict"
+'use strict'
 
 const TypeSource = require('./../../model/source/type')
 const UserSource = require('./../../model/source/user')
@@ -12,163 +12,163 @@ const typeSource = new TypeSource()
 const TypeController = function () { }
 
 TypeController.prototype.getTypes = async (request, response) => {
-    const payload = request.payload
-    var [err, user] = await to(userSource.checkUser(payload.user, payload.password))
-    if (err) {
-        // any error, response unavailable.
-        response.sendStatus(503)
-        return
-    } else if (!user) {
-        // user not found due to invalid information, response unauthorized.
-        response.sendStatus(401)
-        return
-    }
-    var [err, types] = await to(typeSource.getTypes())
-    if (err) {
-        response.sendStatus(503)
-        return
-    }
-    // query successful, response ok with json array of place types.
-    response.json(types)
+  const payload = request.payload
+  var [userErr, user] = await to(userSource.checkUser(payload.user, payload.password))
+  if (userErr) {
+    // any error, response unavailable.
+    response.sendStatus(503)
+    return
+  } else if (!user) {
+    // user not found due to invalid information, response unauthorized.
+    response.sendStatus(401)
+    return
+  }
+  var [queryErr, types] = await to(typeSource.getTypes())
+  if (queryErr) {
+    response.sendStatus(503)
+    return
+  }
+  // query successful, response ok with json array of place types.
+  response.json(types)
 }
 
 TypeController.prototype.getTypeById = async (request, response) => {
-    const payload = request.payload
-    var [err, user] = await to(userSource.checkUser(payload.user, payload.password))
-    if (err) {
-        // any error, response unavailable.
-        response.sendStatus(503)
-        return
-    } else if (!user) {
-        // user not found due to invalid information, response unauthorized.
-        response.sendStatus(401)
-        return
-    }
-    const queryString = request.params
-    var [err, type] = await to(typeSource.getTypeById(queryString.id))
-    if (err) {
-        response.sendStatus(503)
-        return
-    } else if (!type) {
-        // place type not found, response not found.
-        response.sendStatus(404)
-        return
-    }
-    // query successful, response ok with json of place type.
-    response.json(type)
+  const payload = request.payload
+  var [userErr, user] = await to(userSource.checkUser(payload.user, payload.password))
+  if (userErr) {
+    // any error, response unavailable.
+    response.sendStatus(503)
+    return
+  } else if (!user) {
+    // user not found due to invalid information, response unauthorized.
+    response.sendStatus(401)
+    return
+  }
+  const queryString = request.params
+  var [queryErr, type] = await to(typeSource.getTypeById(queryString.id))
+  if (queryErr) {
+    response.sendStatus(503)
+    return
+  } else if (!type) {
+    // place type not found, response not found.
+    response.sendStatus(404)
+    return
+  }
+  // query successful, response ok with json of place type.
+  response.json(type)
 }
 
 TypeController.prototype.addType = async (request, response) => {
-    const payload = request.payload
-    var [err, user] = await to(userSource.checkUser(payload.user, payload.password))
-    if (err) {
-        // any error, response unavailable.
-        response.sendStatus(503)
-        return
-    } else if (!user) {
-        // user not found due to invalid information, response unauthorized.
-        response.sendStatus(401)
-        return
-    } else if (!user.can_insert) {
-        // user not allow to insert, response forbidden.
-        response.sendStatus(403)
-        return
-    }
-    const body = request.body
-    const contentType = request.headers['content-type']
-    const result = new Validator().validatePlaceType(body)
-    if (contentType != CONTENT_TYPE_JSON || result.error) {
-        // content not provided or invalid form of request, response bad request.
-        response.sendStatus(400)
-        return
-    }
-    var [err, newType] = await to(typeSource.addType(body))
-    if (err) {
-        response.sendStatus(503)
-        return
-    }
-    // creation successful, response created.
-    response.status(201)
-        .location(`/types/${newType.type_id}`)
-        .contentType(CONTENT_TYPE_JSON)
-        .send(newType)
+  const payload = request.payload
+  var [userErr, user] = await to(userSource.checkUser(payload.user, payload.password))
+  if (userErr) {
+    // any error, response unavailable.
+    response.sendStatus(503)
+    return
+  } else if (!user) {
+    // user not found due to invalid information, response unauthorized.
+    response.sendStatus(401)
+    return
+  } else if (!user.can_insert) {
+    // user not allow to insert, response forbidden.
+    response.sendStatus(403)
+    return
+  }
+  const body = request.body
+  const contentType = request.headers['content-type']
+  const result = new Validator().validatePlaceType(body)
+  if (contentType !== CONTENT_TYPE_JSON || result.error) {
+    // content not provided or invalid form of request, response bad request.
+    response.sendStatus(400)
+    return
+  }
+  var [addErr, newType] = await to(typeSource.addType(body))
+  if (addErr) {
+    response.sendStatus(503)
+    return
+  }
+  // creation successful, response created.
+  response.status(201)
+    .location(`/types/${newType.type_id}`)
+    .contentType(CONTENT_TYPE_JSON)
+    .send(newType)
 }
 
 TypeController.prototype.updateType = async (request, response) => {
-    const payload = request.payload
-    var [err, user] = await to(userSource.checkUser(payload.user, payload.password))
-    if (err) {
-        // any error, response unavailable.
-        response.sendStatus(503)
-        return
-    } else if (!user) {
-        // user not found due to invalid information, response unauthorized.
-        response.sendStatus(401)
-        return
-    } else if (!user.can_update) {
-        // user not allow to update, response forbidden.
-        response.sendStatus(403)
-        return
-    }
-    const body = request.body
-    const contentType = request.headers['content-type']
-    const queryString = request.params
-    const result = new Validator().validatePlaceType(body)
-    if (queryString.id != body.type_id || contentType != CONTENT_TYPE_JSON || result.error) {
-        // content not provided or invalid form of request, response bad request.
-        response.sendStatus(400)
-        return
-    }
-    var [err, type] = await to(typeSource.getTypeById(queryString.id))
-    if (err) {
-        response.sendStatus(503)
-        return
-    } else if (!type) {
-        // place type to updated not found, response not found.
-        response.sendStatus(404)
-        return
-    }
-    var [err] = await to(typeSource.updateType(body))
-    if (err) {
-        response.sendStatus(503)
-        return
-    }
-    // updation successful, response ok.
-    response.sendStatus(200)
+  const payload = request.payload
+  var [userErr, user] = await to(userSource.checkUser(payload.user, payload.password))
+  if (userErr) {
+    // any error, response unavailable.
+    response.sendStatus(503)
+    return
+  } else if (!user) {
+    // user not found due to invalid information, response unauthorized.
+    response.sendStatus(401)
+    return
+  } else if (!user.can_update) {
+    // user not allow to update, response forbidden.
+    response.sendStatus(403)
+    return
+  }
+  const body = request.body
+  const contentType = request.headers['content-type']
+  const queryString = request.params
+  const result = new Validator().validatePlaceType(body)
+  if (queryString.id !== body.type_id || contentType !== CONTENT_TYPE_JSON || result.error) {
+    // content not provided or invalid form of request, response bad request.
+    response.sendStatus(400)
+    return
+  }
+  var [err, type] = await to(typeSource.getTypeById(queryString.id))
+  if (err) {
+    response.sendStatus(503)
+    return
+  } else if (!type) {
+    // place type to updated not found, response not found.
+    response.sendStatus(404)
+    return
+  }
+  var [updateErr] = await to(typeSource.updateType(body))
+  if (updateErr) {
+    response.sendStatus(503)
+    return
+  }
+  // updation successful, response ok.
+  response.sendStatus(200)
 }
 
 TypeController.prototype.deleteType = async (request, response) => {
-    const payload = request.payload
-    var [err, user] = await to(userSource.checkUser(payload.user, payload.password))
-    if (err) {
-        response.sendStatus(503)
-        return
-    } else if (!user) {
-        // user not found due to invalid information, response unauthorized.
-        response.sendStatus(401)
-        return
-    } else if (!user.can_delete) {
-        // user not allow to delete, response forbidden.
-        response.sendStatus(403)
-        return
-    }
-    const queryString = request.params
-    var [err, type] = await to(typeSource.getTypeById(queryString.id))
-    if (err) {
-        response.sendStatus(503)
-        return
-    } else if (!type) {
-        // place type not found, response not found.
-        response.sendStatus(404)
-        return
-    }
-    var [err] = await to(typeSource.deleteType(queryString.id))
-    if (err) {
-        response.sendStatus(503)
-        return
-    }
-    // deletion successful, response no content.
-    response.sendStatus(204)
+  const payload = request.payload
+  var [userErr, user] = await to(userSource.checkUser(payload.user, payload.password))
+  if (userErr) {
+    response.sendStatus(503)
+    return
+  } else if (!user) {
+    // user not found due to invalid information, response unauthorized.
+    response.sendStatus(401)
+    return
+  } else if (!user.can_delete) {
+    // user not allow to delete, response forbidden.
+    response.sendStatus(403)
+    return
+  }
+  const queryString = request.params
+  var [queryErr, type] = await to(typeSource.getTypeById(queryString.id))
+  if (queryErr) {
+    response.sendStatus(503)
+    return
+  } else if (!type) {
+    // place type not found, response not found.
+    response.sendStatus(404)
+    return
+  }
+  var [deleteErr] = await to(typeSource.deleteType(queryString.id))
+  if (deleteErr) {
+    response.sendStatus(503)
+    return
+  }
+  // deletion successful, response no content.
+  response.sendStatus(204)
 }
 
 module.exports = TypeController
