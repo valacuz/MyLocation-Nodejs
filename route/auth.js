a4c483df10da73e2cf23259aa91a274f2461c68b
Kritpapon Thitichaimongkhol <valacuz@gmail.com>
2018-09-30 01:44:52 +0700

    adjust coding style to conform with standard js

--- route/auth.js
+++ route/auth.js
@@ -1,33 +1,33 @@
 const jwt = require('express-jwt')
 const secret = require('./../config').jwtSecret
 
-function getTokenFromHeader(request) {
-    if (request.headers.authorization &&
-        request.headers.authorization.split(' ')[0] === 'Bearer') {
-        return request.headers.authorization.split(' ')[1]
-    }
-    return null
+function getTokenFromHeader (request) {
+  if (request.headers.authorization &&
+    request.headers.authorization.split(' ')[0] === 'Bearer') {
+    return request.headers.authorization.split(' ')[1]
+  }
+  return null
 }
 
 // Work around when express-jwt throw error because of unauthorized.
 // references: https://github.com/auth0/express-jwt#error-handling
 module.exports.jwtErrorHandling = function (err, request, response, next) {
-    if (err.name === 'UnauthorizedError') {
-        response.sendStatus(401)
-    }
+  if (err.name === 'UnauthorizedError') {
+    response.sendStatus(401)
+  }
 }
 
 module.exports.rules = {
-    required: jwt({
-        secret: secret,
-        credentialsRequired: true,
-        userProperty: 'payload',
-        getToken: getTokenFromHeader
-    }),
-    optional: jwt({
-        secret: secret,
-        credentialsRequired: false,
-        userProperty: 'payload',
-        getToken: getTokenFromHeader
-    })
+  required: jwt({
+    secret: secret,
+    credentialsRequired: true,
+    userProperty: 'payload',
+    getToken: getTokenFromHeader
+  }),
+  optional: jwt({
+    secret: secret,
+    credentialsRequired: false,
+    userProperty: 'payload',
+    getToken: getTokenFromHeader
+  })
 }
