7e6122e6f03101bf3daa7e0f8098ad20f77bf6a6
Kritpapon Thitichaimongkhol <valacuz@gmail.com>
2018-09-30 15:56:07 +0700

    adjust coding style to conform with stadard.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
-const env = require('env2')('./config/local-config.env')
+require('env2')('./config/local-config.env')
 
 module.exports = {
-    // Server configuration here
-    listenPort: process.env.PORT,
-    mongoDbUrl: process.env.MONGO_URL,
-    mongoDbUser: process.env.MONGO_USER,
-    mongoDbPwd: process.env.MONGO_PWD,
-    jwtSecret: process.env.JWT_SECRET
+  // Server configuration here
+  listenPort: process.env.PORT,
+  mongoDbUrl: process.env.MONGO_URL,
+  mongoDbUser: process.env.MONGO_USER,
+  mongoDbPwd: process.env.MONGO_PWD,
+  jwtSecret: process.env.JWT_SECRET
 }
